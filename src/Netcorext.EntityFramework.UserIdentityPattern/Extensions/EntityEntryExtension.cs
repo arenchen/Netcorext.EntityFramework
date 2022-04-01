0fe4c68759318405f5fbf12f0764adb02f9aa72c
Aren Chen <gucci313031@gmail.com>
2022-04-01 15:58:12 +0800

    add ignoreEmptyValue

--- src/Netcorext.EntityFramework.UserIdentityPattern/Extensions/EntityEntryExtension.cs
+++ src/Netcorext.EntityFramework.UserIdentityPattern/Extensions/EntityEntryExtension.cs
@@ -5,11 +5,19 @@ namespace Netcorext.EntityFramework.UserIdentityPattern.Extensions;
 
 public static class EntityEntryExtension
 {
-    public static void UpdateProperty<TEntity, TProperty>(this EntityEntry<TEntity> entry, Expression<Func<TEntity, TProperty>> propertyExpression, TProperty value)
+    public static void UpdateProperty<TEntity, TProperty>(this EntityEntry<TEntity> entry, Expression<Func<TEntity, TProperty>> propertyExpression, TProperty value, bool ignoreEmptyValue = true)
         where TEntity : class
     {
+        switch (ignoreEmptyValue)
+        {
+            case true when value == null:
+            case true when typeof(TProperty) == typeof(string) && string.IsNullOrWhiteSpace("" + value):
+                return;
+        }
+
         if (entry.Property(propertyExpression).OriginalValue == null && value == null) return;
         if (entry.Property(propertyExpression).OriginalValue != null && entry.Property(propertyExpression).OriginalValue!.Equals(value)) return;
+
         entry.Property(propertyExpression).CurrentValue = value;
         entry.Property(propertyExpression).IsModified = true;
     }
