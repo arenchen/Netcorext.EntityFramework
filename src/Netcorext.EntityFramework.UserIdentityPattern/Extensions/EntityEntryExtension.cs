fe8272080e06420575eb1d4c58f6e20f9e2593ba
Aren Chen <gucci313031@gmail.com>
2022-04-06 18:10:43 +0800

    add onchange handler

--- src/Netcorext.EntityFramework.UserIdentityPattern/Extensions/EntityEntryExtension.cs
+++ src/Netcorext.EntityFramework.UserIdentityPattern/Extensions/EntityEntryExtension.cs
@@ -5,20 +5,24 @@ namespace Netcorext.EntityFramework.UserIdentityPattern.Extensions;
 
 public static class EntityEntryExtension
 {
-    public static void UpdateProperty<TEntity, TProperty>(this EntityEntry<TEntity> entry, Expression<Func<TEntity, TProperty>> propertyExpression, TProperty value, bool ignoreEmptyValue = true)
+    public static EntityEntry<TEntity> UpdateProperty<TEntity, TProperty>(this EntityEntry<TEntity> entry, Expression<Func<TEntity, TProperty>> propertyExpression, TProperty value, bool ignoreEmptyValue = true, Action<TEntity>? onChange = null)
         where TEntity : class
     {
         switch (ignoreEmptyValue)
         {
             case true when value == null:
             case true when typeof(TProperty) == typeof(string) && string.IsNullOrWhiteSpace("" + value):
-                return;
+                return entry;
         }
 
-        if (entry.Property(propertyExpression).OriginalValue == null && value == null) return;
-        if (entry.Property(propertyExpression).OriginalValue != null && entry.Property(propertyExpression).OriginalValue!.Equals(value)) return;
+        if (entry.Property(propertyExpression).OriginalValue == null && value == null) return entry;
+        if (entry.Property(propertyExpression).OriginalValue != null && entry.Property(propertyExpression).OriginalValue!.Equals(value)) return entry;
 
         entry.Property(propertyExpression).CurrentValue = value;
         entry.Property(propertyExpression).IsModified = true;
+
+        onChange?.Invoke(entry.Entity);
+
+        return entry;
     }
 }
