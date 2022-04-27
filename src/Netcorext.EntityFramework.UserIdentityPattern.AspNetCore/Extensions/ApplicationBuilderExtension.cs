4ca1d8a868b1aa3bcaf5a2c4da64fa95b99fc0df
Aren Chen <gucci313031@gmail.com>
2022-04-27 16:47:31 +0800

    修改回傳值

--- src/Netcorext.EntityFramework.UserIdentityPattern.AspNetCore/Extensions/ApplicationBuilderExtension.cs
+++ src/Netcorext.EntityFramework.UserIdentityPattern.AspNetCore/Extensions/ApplicationBuilderExtension.cs
@@ -4,36 +4,30 @@ namespace Netcorext.EntityFramework.UserIdentityPattern.AspNetCore;
 
 public static class ApplicationBuilderExtension
 {
-    public static IApplicationBuilder EnsureCreateUserIdentityPatternDatabase(this IApplicationBuilder builder)
+    public static bool EnsureCreateUserIdentityPatternDatabase(this IApplicationBuilder builder)
     {
         using var serviceScope = builder.ApplicationServices.CreateScope();
 
         var context = serviceScope.ServiceProvider.GetRequiredService<DatabaseContext>();
         
-        context.Database.EnsureCreated();
-
-        return builder;
+        return context.Database.EnsureCreated();
     }
     
-    public static IApplicationBuilder EnsureDeleteUserIdentityPatternDatabase(this IApplicationBuilder builder)
+    public static bool EnsureDeleteUserIdentityPatternDatabase(this IApplicationBuilder builder)
     {
         using var serviceScope = builder.ApplicationServices.CreateScope();
 
         var context = serviceScope.ServiceProvider.GetRequiredService<DatabaseContext>();
         
-        context.Database.EnsureDeleted();
-
-        return builder;
+        return context.Database.EnsureDeleted();
     }
     
-    public static IApplicationBuilder MigrateUserIdentityPatternDatabase(this IApplicationBuilder builder)
+    public static void MigrateUserIdentityPatternDatabase(this IApplicationBuilder builder)
     {
         using var serviceScope = builder.ApplicationServices.CreateScope();
 
         var context = serviceScope.ServiceProvider.GetRequiredService<DatabaseContext>();
         
         context.Database.Migrate();
-
-        return builder;
     }
 }
