c59f1828616187e56fb6478f442c10f6abbcff15
Aren Chen <gucci313031@gmail.com>
2022-11-20 15:51:07 +0800

    add SlowCommandLogging

--- src/Netcorext.EntityFramework.UserIdentityPattern.AspNetCore/Extensions/ServiceCollectionExtension.cs
+++ src/Netcorext.EntityFramework.UserIdentityPattern.AspNetCore/Extensions/ServiceCollectionExtension.cs
@@ -1,31 +1,32 @@
 using Microsoft.EntityFrameworkCore;
+using Netcorext.EntityFramework.UserIdentityPattern.Interceptors;
 
 namespace Netcorext.EntityFramework.UserIdentityPattern.AspNetCore;
 
 public static class ServiceCollectionExtension
 {
-    public static IServiceCollection AddIdentityDbContext(this IServiceCollection services, ServiceLifetime lifetime = ServiceLifetime.Scoped)
-        => AddIdentityDbContext<IdentityDbContext>(services, null, lifetime);
+    private const long DEFAULT_SLOW_COMMAND_LOGGING_THRESHOLD = 1000;
+    
+    public static IServiceCollection AddIdentityDbContext(this IServiceCollection services, ServiceLifetime lifetime = ServiceLifetime.Scoped, long slowCommandLoggingThreshold = DEFAULT_SLOW_COMMAND_LOGGING_THRESHOLD)
+        => AddIdentityDbContext<IdentityDbContext>(services, null, lifetime, slowCommandLoggingThreshold);
 
-    public static IServiceCollection AddIdentityDbContext(this IServiceCollection services, Action<IServiceProvider, DbContextOptionsBuilder>? optionsAction, ServiceLifetime lifetime = ServiceLifetime.Scoped)
-        => AddIdentityDbContext<IdentityDbContext>(services, optionsAction, lifetime);
+    public static IServiceCollection AddIdentityDbContext(this IServiceCollection services, Action<IServiceProvider, DbContextOptionsBuilder>? optionsAction, ServiceLifetime lifetime = ServiceLifetime.Scoped, long slowCommandLoggingThreshold = DEFAULT_SLOW_COMMAND_LOGGING_THRESHOLD)
+        => AddIdentityDbContext<IdentityDbContext>(services, optionsAction, lifetime, slowCommandLoggingThreshold);
 
-    public static IServiceCollection AddIdentityDbContext<TContext>(this IServiceCollection services, ServiceLifetime lifetime = ServiceLifetime.Scoped)
-        where TContext : DatabaseContext => AddIdentityDbContext<TContext>(services, null, lifetime);
+    public static IServiceCollection AddIdentityDbContext<TContext>(this IServiceCollection services, ServiceLifetime lifetime = ServiceLifetime.Scoped, long slowCommandLoggingThreshold = DEFAULT_SLOW_COMMAND_LOGGING_THRESHOLD)
+        where TContext : DatabaseContext => AddIdentityDbContext<TContext>(services, null, lifetime, slowCommandLoggingThreshold);
 
-    public static IServiceCollection AddIdentityDbContext<TContext>(this IServiceCollection services, Action<IServiceProvider, DbContextOptionsBuilder>? optionsAction, ServiceLifetime lifetime = ServiceLifetime.Scoped)
+    public static IServiceCollection AddIdentityDbContext<TContext>(this IServiceCollection services, Action<IServiceProvider, DbContextOptionsBuilder>? optionsAction, ServiceLifetime lifetime = ServiceLifetime.Scoped, long slowCommandLoggingThreshold = DEFAULT_SLOW_COMMAND_LOGGING_THRESHOLD)
         where TContext : DatabaseContext
     {
         services.AddHttpContextAccessor();
 
-        if (optionsAction == null)
-        {
-            services.AddDbContext<DatabaseContext, TContext>(lifetime);
-        }
-        else
-        {
-            services.AddDbContext<DatabaseContext, TContext>(optionsAction, lifetime);
-        }
+        services.AddDbContext<DatabaseContext, TContext>((provider, builder) =>
+                                                         {
+                                                             var loggerFactory = provider.GetRequiredService<ILoggerFactory>();
+                                                             builder.AddInterceptors(new SlowCommandLoggingInterceptor(loggerFactory, slowCommandLoggingThreshold));
+                                                             optionsAction?.Invoke(provider, builder);
+                                                         }, lifetime);
 
         return services;
     }
---
+++ src/Netcorext.EntityFramework.UserIdentityPattern/Interceptors/SlowCommandLoggingInterceptor.cs
@@ -0,0 +1,67 @@
+using System.Data.Common;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Logging;
+
+namespace Netcorext.EntityFramework.UserIdentityPattern.Interceptors;
+
+public class SlowCommandLoggingInterceptor : DbCommandInterceptor
+{
+    private readonly long _slowCommandLoggingThreshold;
+    private readonly ILogger<SlowCommandLoggingInterceptor> _logger;
+
+    public SlowCommandLoggingInterceptor(ILoggerFactory loggerFactory, long slowCommandLoggingThreshold)
+    {
+        _slowCommandLoggingThreshold = slowCommandLoggingThreshold;
+        _logger = loggerFactory.CreateLogger<SlowCommandLoggingInterceptor>();
+    }
+
+    public override DbDataReader ReaderExecuted(DbCommand command, CommandExecutedEventData eventData, DbDataReader result)
+    {
+        LogSlowCommand(command.CommandText, eventData.Duration);
+
+        return base.ReaderExecuted(command, eventData, result);
+    }
+
+    public override ValueTask<DbDataReader> ReaderExecutedAsync(DbCommand command, CommandExecutedEventData eventData, DbDataReader result, CancellationToken cancellationToken = new CancellationToken())
+    {
+        LogSlowCommand(command.CommandText, eventData.Duration);
+
+        return base.ReaderExecutedAsync(command, eventData, result, cancellationToken);
+    }
+
+    public override object? ScalarExecuted(DbCommand command, CommandExecutedEventData eventData, object? result)
+    {
+        LogSlowCommand(command.CommandText, eventData.Duration);
+        
+        return base.ScalarExecuted(command, eventData, result);
+    }
+
+    public override ValueTask<object?> ScalarExecutedAsync(DbCommand command, CommandExecutedEventData eventData, object? result, CancellationToken cancellationToken = new CancellationToken())
+    {
+        LogSlowCommand(command.CommandText, eventData.Duration);
+        
+        return base.ScalarExecutedAsync(command, eventData, result, cancellationToken);
+    }
+
+    public override int NonQueryExecuted(DbCommand command, CommandExecutedEventData eventData, int result)
+    {
+        LogSlowCommand(command.CommandText, eventData.Duration);
+        
+        return base.NonQueryExecuted(command, eventData, result);
+    }
+
+    public override ValueTask<int> NonQueryExecutedAsync(DbCommand command, CommandExecutedEventData eventData, int result, CancellationToken cancellationToken = new CancellationToken())
+    {
+        LogSlowCommand(command.CommandText, eventData.Duration);
+        
+        return base.NonQueryExecutedAsync(command, eventData, result, cancellationToken);
+    }
+
+    private void LogSlowCommand(string commandText, TimeSpan duration)
+    {
+        if (duration.TotalMilliseconds > _slowCommandLoggingThreshold)
+        {
+            _logger.LogWarning("Slow command: {CommandText} ({Duration})", commandText, duration);
+        }
+    }
+}
